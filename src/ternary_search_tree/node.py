91f086ee71629e81f81b619d021d4cc0852caac9
Dorothy Chepkoech <dorothy.chepkoech@student.uhasselt.be>
2025-04-22 09:31:39 +0300

    Implement insert and search with manual test verification

---
+++ src/ternary_search_tree/node.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.left = None
+        self.middle = None
+        self.right = None
+        self.is_end_of_word = False
