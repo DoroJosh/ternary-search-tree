c561f7deed48d08604d08aee2c8ade8936bcb5aa
mariliabezrra <mariliamaria.daconceicaobezerra@student.uhasselt.be>
2025-05-19 21:42:29 +0200

    Setup the main structure of Insert
    This file has only the structure of the insert algorithm with the main functions

---
+++ src/ternary_search_tree/Insertion_TST.py
@@ -0,0 +1,16 @@
+#Ternary search tree 
+
+class TenaryTreePointers:
+    def __init__(self, a, b, c):
+        self.left = a # Left: points to the node whose value is less than the value in the current node
+        self.equal = b # Equal: points to the node whose value is equal to the value in the current node
+        self.right = c # Right: points to the node whose value is greater than the value in the current node
+
+class TernaryTreeInsertion: 
+    def ___init___(self): 
+        self.root = None # Starting the tree with no nodes 
+
+    def insertMain(self,word): 
+        # Add here the main function 
+    
+    def insertionAuxiliar(self,node,word): # self: gives reference to the tree itself; node: current node; work: new work to be add; 
